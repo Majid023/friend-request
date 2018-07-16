13b84fb280fbd7a650521c78ea97637c6fd540d8
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-07-16 20:48:48 +0530

    update

--- app/public/vuejs/vuecustom.js
+++ app/public/vuejs/vuecustom.js
@@ -1,25 +1,32 @@
 
+var data1 = new Vue({
 
     el : '#app',
 
-        },
-    data :  {
-                message : "Hello world",
-                datastorage : [],
-                isActive :false
-            },
-
 	methods : {
+                buttonClicked: function() {
 
-                buttonClicked: function(event){
-                        axios.post('/friendlist', { data: this.data }).then( function( response ) {
-                                console.log(response);
-                                this.datastorage=response.data;
-                        }).catch(  function(error) {
-                             // Wu oh! Something went wrong
-                            console.log(error.message);
-                        });
-                    }   
-                },
+                    var datahandle = document.getElementById("app");
+                    var str="";
+                    var str1="<div class='carousel' data-flickity='group-cell'>";
+                 axios.post('/friendlist', { data: this.datastorage }).then(function(response){
+                         for ( var i=0;i<response.data.length;i++){
+                             str+="<div class='carousel-cell'>"+
+                               "<h1>" + response.data[i].Id + "</h1>" +
+                               "<h6>" + response.data[i].Name + "</h6>" +
+                               "<h6>" + response.data[i].City + "</h6>" +
+                               "<h6>" + response.data[i].Birth + "</h6>" +
+                             "</div>"
+                         }
+                         datahandle.innerHTML = str1 +str+"</div>";
+                        console.log(response.data);
+
+                    }).catch(  function(error) {
+                        console.log(error.message);  
+                    });
+                    
+                }
+            }
 
 });
+
